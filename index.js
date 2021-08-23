b655b46e5236c334342fea2d8e438977876d05be
Jacob R <jakeriches1@gmail.com>
2021-08-23 16:43:58 +0100

    Linted code

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
-const http = require('http');
-const fs = require('fs');
+import { createServer } from 'http';
+import { readFileSync, readFile } from 'fs';
 
-const server = http.createServer(function (req, res) {
-  const myURL = new URL(req.url, 'https://localhost:3000');
-  returnPage(myURL.pathname, res)
-})
-
-const page404 = fs.readFileSync("404.html", "utf-8", (err, data) => {
+const page404 = readFileSync('404.html', 'utf-8', (err, data) => {
   if (err) throw err;
   return data;
 });
 
 const returnPage = (path, res) => {
-  let cleansedPath = path.replace('/', '')
-  if(cleansedPath === ''){
+  let cleansedPath = path.replace('/', '');
+  if (cleansedPath === '') {
     cleansedPath = 'index.html';
   }
 
-  fs.readFile(cleansedPath, 'utf8', function(err, data){ 
-    res.setHeader('Content-Type', 'text/html')
+  readFile(cleansedPath, 'utf8', (err, data) => {
+    res.setHeader('Content-Type', 'text/html');
 
-    if(err){
-      res.statusCode = 404
-      res.write(page404)    
-    }else if(data){
-      res.statusCode = 200
-      res.write(data)
+    if (err) {
+      res.statusCode = 404;
+      res.write(page404);
+    } else if (data) {
+      res.statusCode = 200;
+      res.write(data);
     }
 
-    res.end()
-  })
-}
+    res.end();
+  });
+};
 
-server.listen(3000, () => {
-  console.log('Server started! --- listening on port 3000')
+createServer((req, res) => {
+  const myURL = new URL(req.url, 'https://localhost:3000');
+  returnPage(myURL.pathname, res);
 });
